b8668f265f92b01671a266785dba81967b9955e3
Nitro <2484435@cegepmv.ca>
2026-03-24 10:17:37 -0400

    adoption spec

--- myproject/src/Module/adoption/adoption.module.ts
+++ myproject/src/Module/adoption/adoption.module.ts
@@ -3,6 +3,8 @@ import { AdoptionController } from './adoption/adoption.controller';
 import { FormsController } from './forms/forms.controller';
 import { AdoptionService } from './adoption/adoption.service';
 import { FormsService } from './forms/forms.service';
+import { AdoptionService } from './adoption/adoption.service';
+import { AdoptionController } from './adoption/adoption.controller';
 
 @Module({
   controllers: [AdoptionController, FormsController],
