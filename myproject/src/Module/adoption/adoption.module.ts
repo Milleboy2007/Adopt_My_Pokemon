6643194829e2a57dcdaaa2c17ccc1f8a56118a0e
Nitro <2484435@cegepmv.ca>
2026-03-24 10:31:42 -0400

    form spec

--- myproject/src/Module/adoption/adoption.module.ts
+++ myproject/src/Module/adoption/adoption.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AdoptionController } from './adoption/adoption.controller';
-import { FormsController } from './forms/forms.controller';
 import { AdoptionService } from './adoption/adoption.service';
 import { FormsService } from './forms/forms.service';
-import { AdoptionService } from './adoption/adoption.service';
-import { AdoptionController } from './adoption/adoption.controller';
+import { FormsController } from './forms/forms.controller';
 
 @Module({
   controllers: [AdoptionController, FormsController],
