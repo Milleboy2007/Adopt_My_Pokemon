4581e81cdf2cbe3dbfc5a61c5e670db4d17d2253
Nitro <2484435@cegepmv.ca>
2026-03-31 11:02:17 -0400

    correction

--- myproject/src/Module/quiz/quiz.module.ts
+++ myproject/src/Module/quiz/quiz.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { QuizController } from './quiz/quiz.controller';
 import { QuizService } from './quiz/quiz.service';
-import { QuizService } from './quiz/quiz.service';
-import { QuizController } from './quiz/quiz.controller';
 
 @Module({
   controllers: [QuizController],
