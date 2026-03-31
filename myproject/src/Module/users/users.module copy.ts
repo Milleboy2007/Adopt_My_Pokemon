98d3fbc6a3d3aa2889f4939e831227f2a68b3162
Nitro <2484435@cegepmv.ca>
2026-03-31 12:11:22 -0400

    correction

--- myproject/src/Module/users/users.module copy.ts
+++ myproject/src/Module/users/users.module copy.ts
@@ -2,7 +2,7 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UsersService } from './services/users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user.entity';
+import { User } from './entities/user.entity';
 import { AuthService } from './services/auth.service';
 import { CurrentUserMiddleware } from './middlewares/current-user.middleware';
 
