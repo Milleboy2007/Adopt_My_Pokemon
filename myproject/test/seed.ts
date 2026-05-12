523ec0b3d35f7eeeef603d4641a31884921124b7
Nitro <2484435@cegepmv.ca>
2026-05-12 11:37:30 -0400

    seed

--- myproject/test/seed.ts
+++ myproject/test/seed.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
 import { AuthService } from '../src/Module/users/services/auth.service';
 import { DataSource } from 'typeorm';
+import { PokemonService } from 'src/Module/pokemon/service/pokemon.service';
+import { QuizService } from 'src/Module/quiz/quiz/quiz.service';
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
   
   const dataSource = app.get(DataSource);
   const authService = app.get(AuthService);
+  const pokemonServie = app.get(PokemonService)
+  const quizService = app.get(QuizService)
 
   console.log('🌱 Démarrage du script de seed...');
 
@@ -27,6 +31,20 @@ async function bootstrap() {
     // Création d'un utilisateur normal (Niveau 1 par défaut)
     await authService.signUp('test@test.com', 'test');
     console.log('👤 Utilisateur normal créé : test@test.com');
+
+    console.log("Load des pokemon a partire de l'api")
+    await pokemonServie.getAllAPIPoke(500)
+
+    console.log('Creation de quiz')
+    await quizService.createQuiz("Quiz - Pokémon-Easy", 'EASY', 10, 5)
+    await quizService.createQuiz("Quiz - Pokémon-Easy", 'EASY', 10, 5)
+    await quizService.createQuiz("Quiz - Pokémon-Easy", 'EASY', 10, 5)
+    await quizService.createQuiz("Quiz - Pokémon-Medium", 'Medium', 25, 5)
+    await quizService.createQuiz("Quiz - Pokémon-Medium", 'Medium', 25, 5)
+    await quizService.createQuiz("Quiz - Pokémon-Medium", 'Medium', 25, 5)
+    await quizService.createQuiz("Quiz - Pokémon-Hard", 'HARD', 50, 5)
+    await quizService.createQuiz("Quiz - Pokémon-Hard", 'HARD', 50, 5)
+    await quizService.createQuiz("Quiz - Pokémon-Hard", 'HARD', 50, 5)
 
     console.log('✅ Base de données initialisée avec succès !');
 
